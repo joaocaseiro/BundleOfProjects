ed71e4f8be4d30d48242bac9e1de957213b10ab4
jcaseiro <jmvcaseiro@gmail.com>
2012-05-31 09:31:02 +0100

    Removed some useless classes.

--- UtilitiesForAndroid/MyPrettyProject/src/my/pretty/project/MyPrettyProjectActivity.java
+++ UtilitiesForAndroid/MyPrettyProject/src/my/pretty/project/MyPrettyProjectActivity.java
@@ -9,7 +9,5 @@ public class MyPrettyProjectActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        
     }
 }
